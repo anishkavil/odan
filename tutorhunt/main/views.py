dc1b0e3b85a2f03a798c26ef63479ecc6b415050
Anish PK <anish.kavil@gmail.com>
2016-03-25 04:28:34 +0530

    Added mappings for the index files

--- tutorhunt/main/views.py
+++ tutorhunt/main/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 # Create your views here.
 
 def index(request):
     return HttpResponse("This is the home page for Tutor Hunt")
 
+def home(request):
+    # Add anything that you want to add to the context
+    context = {
+        'k1': "latest_question_list",
+    }
+    
+    template = loader.get_template('index.html')
+    return HttpResponse(template.render(context, request))
 
--- tutorhunt/tutorhunt/settings.py
+++ tutorhunt/tutorhunt/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'tutorhunt.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [ os.path.join(BASE_DIR, 'main/templates/main'),],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,5 +120,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "main/static"),
+    os.path.join(BASE_DIR, "main/static/main"),
 ]
