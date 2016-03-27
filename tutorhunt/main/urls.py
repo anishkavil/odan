947ebf5ea709fd2e9bcd482ae5d817288714fe12
Anish PK <anish.kavil@gmail.com>
2016-03-27 10:55:08 +0530

    added few other files with bootstrap settings

--- tutorhunt/main/urls.py
+++ tutorhunt/main/urls.py
@@ -4,6 +4,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$',views.home,name='home'),
+    url(r'^$',views.home,name='index'),
     #url(r'home/$',views.home,name='home'),
 ]
