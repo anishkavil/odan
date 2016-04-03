2a47fc359d0fc7cea5f4aed7635908717179e2cc
Anish PK <anish.kavil@gmail.com>
2016-04-03 19:00:33 +0530

    Improved pages
    Basic login and about us pages added.

--- tutorhunt/main/views.py
+++ tutorhunt/main/views.py
@@ -16,3 +16,24 @@ def home(request):
     template = loader.get_template('main/index.html')
     return HttpResponse(template.render(context, request))
 
+
+def bootstraphome(request):
+    cntxt = {
+             'rndmTxt' : 'The quick brown fox jumps over the little lazy dog',
+             }
+    template = loader.get_template('main/indexbasic.html')
+    return HttpResponse(template.render(cntxt,request))
+
+def aboutus(request):
+    cntxt = {
+             'rndmTxt' : 'The quick brown fox jumps over the little lazy dog',
+             }
+    template = loader.get_template('main/aboutus.html')
+    return HttpResponse(template.render(cntxt,request))
+
+def contactus(request):
+    cntxt = {
+             'rndmTxt' : 'The quick brown fox jumps over the little lazy dog',
+             }
+    template = loader.get_template('main/contactus.html')
+    return HttpResponse(template.render(cntxt,request))
